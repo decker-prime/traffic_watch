251c88aa691e7f406722e35cf5794fc244ba4d2c
Phillip Decker <pdecker999@gmail.com>
2020-02-26 07:37:26 -0500

    regular status timer update

--- traffic_watch.py
+++ traffic_watch.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
 
 import argparse
-import pprint
 import psutil
 from functools import partial
-from scapy.layers.http import HTTP, HTTPRequest
 from multiprocessing import Process, Queue, Pipe
 import multiprocessing
 import time
-import pandas
 
+import pandas as pd
+from apscheduler.schedulers.background import BackgroundScheduler
+from pandas import DataFrame
 from scapy.all import sniff, bind_layers, IP, TCP
-
-
-import sys
-import pdb
-
-class ForkedPdb(pdb.Pdb):
-    """A Pdb subclass that may be used
-    from a forked multiprocessing child
-
-    """
-    def interaction(self, *args, **kwargs):
-        _stdin = sys.stdin
-        try:
-            sys.stdin = open('/dev/stdin')
-            pdb.Pdb.interaction(self, *args, **kwargs)
-        finally:
-            sys.stdin = _stdin
+from scapy.layers.http import HTTP, HTTPRequest
 
 loopback_buffer = set()
 
@@ -66,13 +50,47 @@ def the_packet(packet, comm_queue):
     #ForkedPdb().set_trace()
 
     fields = packet.getlayer(HTTPRequest).fields
-    #pprint.pprint(packet.getlayer(TCP).fields, indent=4)
-    #pprint.pprint(fields, indent=4)
+    section = fields['Path'].decode('utf-8').split("/")
     
+    if len(section)>0:
+        section = f'/{section[1]}'
+    else:
+        section = '/'
+
     comm_queue.put({'time':recv_time
                     ,'src_ip':packet.getlayer(IP).getfieldval('src')
-                    ,'path':fields['Path'].decode('utf-8')
+                    ,'path':section
                     })
+
+def recent_activity(records, threshold_secs=10):
+    now = time.time()
+    # This was originally a list comprehension, but since the records are in
+    # time order, breaking the iteration at the time threshold is easier to
+    # read in this format
+    records_to_check = []
+    for i in reversed(records):
+        if i['time'] > now - threshold_secs:
+            records_to_check.append(i)
+        else:
+            break
+    
+    if len(records_to_check) > 0:
+        df = DataFrame(records_to_check)
+        vals = df.groupby(['path']).size().sort_values(ascending=False)
+        
+        message = "Most popular section"
+        message += "s: " if len(vals)> 1 else ": "
+
+        for section, hits in zip(vals.index, vals.values):
+            message += f'{section}: {hits} '
+            message += "hits, " if hits > 1 else "hit, "
+        
+        message += f'{len(records_to_check) / threshold_secs} requests/sec'
+    else:
+        message = "No recent activity..."
+
+    print(message, end='\r', flush=True)
+    
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
@@ -90,6 +108,10 @@ if __name__ == '__main__':
             bind_layers(TCP,HTTP, dport=args.port) 
             bind_layers(TCP,HTTP, sport=args.port)
 
+    traffic_records = []
+
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(recent_activity, 'interval', seconds=10, args=(traffic_records,))
 
     # this is the queue for receiving info from the network-sniffing subprocess
     incoming_data_queue = multiprocessing.Queue()
@@ -97,10 +119,33 @@ if __name__ == '__main__':
     sniffer = Process(target=run_sniffer, args=(args.port,incoming_data_queue))
     sniffer.daemon = True
     sniffer.start()
+    scheduler.start()
     
-    traffic_records = []
+    #counter = 0
     while True:
             new_traffic = incoming_data_queue.get()
             traffic_records.append(new_traffic)
-            print(new_traffic, end='\r', flush=True)
-    
+            #print(new_traffic, end='\r', flush=True)
+    #        counter  = counter+1
+    #        if counter > 4:
+    #            break
+
+    #recent_activity(traffic_records) 
+
+'''
+import sys
+import pdb
+
+class ForkedPdb(pdb.Pdb):
+    """A Pdb subclass that may be used
+    from a forked multiprocessing child
+
+    """
+    def interaction(self, *args, **kwargs):
+        _stdin = sys.stdin
+        try:
+            sys.stdin = open('/dev/stdin')
+            pdb.Pdb.interaction(self, *args, **kwargs)
+        finally:
+            sys.stdin = _stdin
+'''
