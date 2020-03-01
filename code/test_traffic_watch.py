d77d9f44a305cf0c3d30cb3b828ef11807a59123
Phillip Decker <pdecker999@gmail.com>
2020-03-01 06:19:30 -0500

    Tests updated to reflect the default traffic per sec and new commentary

--- code/test_traffic_watch.py
+++ code/test_traffic_watch.py
@@ -20,6 +20,10 @@ class Test(TestCase):
     alert_threshold = 8
 
     def test_traffic_alert_no_traffic(self):
+        """
+        This tests the case where there's no traffic, so there shouldn't be
+        any alerting
+        """
         alert = TrafficAlert()
         test_traffic = collections.deque()
         alert.traffic_alert(test_traffic, self.alert_threshold,
@@ -27,35 +31,53 @@ class Test(TestCase):
         self.assertFalse(alert.alert_engaged)
 
     def test_traffic_alert_below_threshold(self):
+        """
+        This tests the case where there is traffic, but it remains below the
+        alert levels. The alert should remain disengaged
+        """
         alert = TrafficAlert()
         test_traffic = collections.deque()
-        for i in range(self.alert_threshold - 1):
+        # fake up some traffic
+        for i in range(self.alert_threshold*self.alert_period - 1):
             test_traffic.append({'time': time.time(),
                                  'src_ip': '0.0.0.0',
                                  'path': '/'})
+        # trigger the alert check, like the scheduler does
         alert.traffic_alert(test_traffic, self.alert_threshold,
                             self.alert_period)
         self.assertFalse(alert.alert_engaged)
 
     def test_traffic_alert_above_threshold(self):
+        """
+        This test checks that the alert fires properly when the amount of
+        traffic exceeds the threshold
+        """
         alert = TrafficAlert()
         test_traffic = collections.deque()
-        base_rate = (self.alert_threshold + 1)
+        base_rate = (self.alert_threshold * self.alert_period + 1)
+        # fake up some traffic
         for i in range(base_rate):
             test_traffic.append({'time': time.time(),
                                  'src_ip': '0.0.0.0',
                                  'path': '/'})
+        # trigger the alert check, like the scheduler does
         alert.traffic_alert(test_traffic, self.alert_threshold,
                             self.alert_period)
         self.assertTrue(alert.alert_engaged)
 
     def test_traffic_alert_shuts_off_after_alert(self):
+        """
+        This test checks that the alert successfully returns to a non-alert
+        state after the alert traffic conditions clear
+        """
         alert = TrafficAlert()
         test_traffic = collections.deque()
-        for i in range(self.alert_threshold + 1):
+        # fake up some traffic
+        for i in range(self.alert_threshold*self.alert_period + 1):
             test_traffic.append({'time': time.time(),
                                  'src_ip': '0.0.0.0',
                                  'path': '/'})
+        # trigger the alert check, like the scheduler does
         alert.traffic_alert(test_traffic, self.alert_threshold,
                             self.alert_period)
         self.assertTrue(alert.alert_engaged)
